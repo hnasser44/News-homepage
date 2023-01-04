81a0169739cd14d77fa091ea56cfe9fd60e9274e
hnasser44 <hassan.nasser.2@st.ul.edu.lb>
2023-01-04 07:00:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const sideMenu = document.querySelector('.side-menu');
+const menuOpenIcon = document.querySelector('.header-menu');
+const closeMenuIcon = document.querySelector('.close-menu');
+const Nav = document.querySelector('nav');
+
+menuOpenIcon.addEventListener('click', () => {
+  sideMenu.classList.remove('hidden');
+})
+
+closeMenuIcon.addEventListener('click', () => {
+  sideMenu.classList.add('hidden');
+})
+
+//window resize
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    sideMenu.classList.add('hidden');
+    Nav.classList.remove('hidden');
+    menuOpenIcon.classList.add('hidden');
+  }
+  else {
+    Nav.classList.add('hidden');
+    menuOpenIcon.classList.remove('hidden');
+  }
+})
